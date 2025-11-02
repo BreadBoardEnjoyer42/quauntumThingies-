3ddb45d0c812e47fb894016ea7effa18c8ba6fde
BreadBoardEnjoyer42 <bowmanw060@gmail.com>
2025-11-01 21:01:06 -0400

    Update Main Code.c
    Worked on and got a starting place for the back end work. Lots of work to do from here

--- Main Code.c
+++ Main Code.c
@@ -1,7 +1,86 @@
-#include <stdio.h>
+//==========================================
+// Name: BattleShipCPE223
+// Author: Wyatt Bowman
+// Date: 11/1/25
+// Version: V0.1
+// Description:
+// Details: UPDATE TO FGETS
+//==========================================
 
-int main(){
-    printf("Hello World");
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // Required to use strings
+#include <stdlib.h> // Required for system()
+
+int setBoats(int, int, int, int);
+void showBoard(); // declaring function that shows board
+
+int BattleArray[11][11] = {0}; // declaring array, indexing starts with one
+
+
+int main()
+{
+    int userX, userY;
+    char Ships[5][20] = {"Carrier", "Battleship", "Submarine", "Cruiser", "Destroyer"}; // names and such
+    int rotate;
+
+
+// This code is just for develpoment  
+    for(int i = 0; i <= 10; i++){ // fill with zeros and creating margins
+        printf("\n\n");
+        for(int j = 0; j <= 10; j++){
+
+            if(i == 0){
+                BattleArray[i][j] = j;
+            } else if (j == 0){
+                BattleArray[i][j] = i;
+            } else{
+                BattleArray[i][j] = 0;
+            }
+
+             printf("%d  ", BattleArray[i][j]);
+        }
+    }
+
+     printf("\n");
+     for(int boat = 0; boat <= 4; boat++){
+        printf("Where do you want your %s to start in the X:    ", Ships[boat]); // literally just going to assume bottom corner and horizontally for now
+        scanf("%d", &userX);
+        printf("Where do you want your %s to start in the Y:    ", Ships[boat]); // Y coord
+        scanf("%d", &userY);
+        printf("Do you want your piece Vertically: "); // one for vertical, in future take more input
+        scanf("%d", &rotate);
+        printf("\n");
+        setBoats(userX, userY, boat, rotate); // boats to control index of the actual piece
+     }
+    showBoard(); // Showing the board
+
+    while(1){} // THIS IS TEMP PLEASE DON'T LEAVE THIS WILL BREAK COMPUTER OVER TIME
 
     return 0;
 }
+
+int setBoats(int xCoord, int yCoord, int boatIndex, int orientation){ //placing boats
+
+    int length[5] = {5, 4, 3, 3, 2}; // lengths of each piece
+
+    if(orientation == 1){
+        for (int i = 0; i < length[boatIndex]; i++) {
+            BattleArray[xCoord + i][yCoord] = 1; // placing boats in array with size
+        }
+    }else{} // insert horizontal code here after fixing filter statement for vert
+    return 1;
+}
+
+
+void showBoard(){ // shows board, makes sense ig
+    system("cls"); // clears terminal
+for(int i = 0; i <= 10; i++){
+        printf("\n\n");
+        for(int j = 0; j <= 10; j++){
+             printf("%d  ", BattleArray[i][j]);
+        }
+    }
+}
+
+
